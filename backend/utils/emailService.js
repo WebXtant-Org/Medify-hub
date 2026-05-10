31de70eddcf54ef26acfe7ff12e3f2bd23a703be
WebXtant <webxtant.services@gmail.com>
2026-05-10 10:57:26 +0530

    Fix: email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -9,15 +9,16 @@ const getTransporter = () => {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
       tls: {
         rejectUnauthorized: false
-      }
+      },
+      family: 4
     });
   }
   return transporter;
