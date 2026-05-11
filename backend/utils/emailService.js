4b0bb394780a9b05cdba95bfb031c9c5877d9838
WebXtant <webxtant.services@gmail.com>
2026-05-11 22:50:06 +0530

    fix: switch to port 587 with starttls

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -15,8 +15,8 @@ const getTransporter = async () => {
 
     transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // use SSL
+      port: 587,
+      secure: false, // use STARTTLS
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
